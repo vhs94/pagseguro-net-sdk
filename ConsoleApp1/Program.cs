0fbfb8c562d67ba9f69237ecd8c9af61c3ecf894
Victor Hugo Ramos <vsantana@aubay.pt>
2023-07-22 20:26:25 +0100

    force workflow run

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,18 +2,6 @@
 using PagSeguro.DotNet.Sdk.Common.Settings;
 using PagSeguro.DotNet.Sdk.Connect.Dtos.Authorization.Challenge;
 using PagSeguro.DotNet.Sdk.Settings;
-using System.Security.Cryptography;
-
-var rsa = RSA.Create(2048);
-//var pubKey = rsa.ExportSubjectPublicKeyInfo();
-//var privKey = rsa.ImportRSAPublicKey();
-
-//Console.WriteLine(Convert.ToBase64String(pubKey));
-//Console.WriteLine("-------------");
-//Console.WriteLine(Convert.ToBase64String(privKey));
-
-
-
 
 var pagSeguro = new PagSeguroClient(new ClientSettings
 {
@@ -29,18 +17,4 @@ await pagSeguro.ConnectChallengeAsync(new ChallengeWriteDto
 });
 
 var digitalCertificate = await pagSeguro.DigitalCertificate.CreateCertificateAsync();
-var teste = digitalCertificate.Key;
-
-
-
-var app = await pagSeguro.Application.GetApplicationAsync("8815a5e2-616b-4754-a6d1-40d92b71674c");
-//var challenge = await pagSeguro.Connect.Authorization.CreateAccessTokenByChallengeAsync(new ChallengeWriteDto
-//{
-//    ClientId = "8815a5e2-616b-4754-a6d1-40d92b71674c",
-//    ClientSecret = "45e5a4de-b8eb-4b78-9ce6-fad416b1953c"
-//});
-
-
-
-//Console.WriteLine(JsonConvert.SerializeObject(challenge));
 Console.ReadKey();
