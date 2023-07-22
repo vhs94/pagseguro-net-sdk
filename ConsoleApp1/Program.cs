4820c11235a65b7c383bc45b9b5ecc842e69e814
Victor Hugo Ramos <vsantana@aubay.pt>
2023-07-22 20:30:50 +0100

    test pr

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,6 +15,5 @@ await pagSeguro.ConnectChallengeAsync(new ChallengeWriteDto
     ClientId = "8815a5e2-616b-4754-a6d1-40d92b71674c",
     ClientSecret = "45e5a4de-b8eb-4b78-9ce6-fad416b1953c"
 });
-
 var digitalCertificate = await pagSeguro.DigitalCertificate.CreateCertificateAsync();
 Console.ReadKey();
